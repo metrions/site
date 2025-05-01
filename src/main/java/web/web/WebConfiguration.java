874435f9f49087463bc24677c4fa80bc576922e9
PANARIN <panarin@PANARIN.panarin>
2025-05-01 14:51:34 +0700

    обновил билд

--- src/main/java/web/web/WebConfiguration.java
+++ src/main/java/web/web/WebConfiguration.java
@@ -16,8 +16,8 @@ public class WebConfiguration implements WebMvcConfigurer {
                         frontend,
                         "http://localhost:3000",
                         "https://panarin.site:3000",
-                        "https://9b6d-2a0d-6c2-17-8038-00.ngrok-free.app",
-                        "https://*"
+                        "https://b9c2-2a03-6f02-00-8920.ngrok-free.app",
+                        "https://94.241.174.238"
                 )
                 .allowedMethods("*")
                 .allowCredentials(true); // Если используется авторизация через cookie
